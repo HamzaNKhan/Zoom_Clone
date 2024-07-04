600674068f572b27bb6696073f5ec03dc39a5d13
Hamza <hamzanadeem170398@gmail.com>
2024-07-04 15:09:53 +0500

    Sidebar - Pages boilers

---
+++ constants.ts
@@ -0,0 +1,13 @@
+export const sidebarLinks = [
+
+        { imgUrl: "/icons/home.svg", route: "/", label: "Home" },
+        
+        { imgUrl: "/icons/upcoming.svg", route: "/upcoming", label: "Upcoming" },
+
+        { imgUrl: "/icons/previous.svg", route: "/previous", label: "Previous" },
+
+        { imgUrl: "/icons/video.svg", route: "/recording", label: "Video" },
+
+        { imgUrl: "/icons/add-personal.svg", route: "/personal-room", label: "Personal Room" },
+
+]
